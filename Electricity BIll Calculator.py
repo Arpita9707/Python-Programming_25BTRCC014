09462f830380ae9d5fd70acd6ffccd9882a38c08
unknown <juug25btech14725@jainuniversirty.ac.in>
2026-08-14 19:05:30 +0530

    Added second project

---
+++ Electricity BIll Calculator.py
@@ -0,0 +1,21 @@
+
+n = int(input("Enter number of consumers: "))
+for i in range(1, n + 1):
+    name = input(f"Consumer {i}: ")
+    units = int(input("Units: "))
+    if units == 0:
+        print("0 units entered. Processing stopped.")
+        break
+    if units < 0:
+        print(f"Invalid units for {name} - Skipped")
+        continue
+    if units <= 100:
+        rate = 2
+    elif units <= 200:
+        rate = 3
+    elif units <= 300:
+        rate = 5
+    else:
+        rate = 7
+    bill = units * rate
+    print(f"{name} - {units} units - Bill: ₹{bill}")
